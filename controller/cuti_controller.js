b9040a49677ea2c0361fcad128a67cb794f1f050
Mutia Meilani <87632337+seaaralee@users.noreply.github.com>
2022-09-09 15:28:27 +0700

    api karyawan

--- controller/cuti_controller.js
+++ controller/cuti_controller.js
@@ -36,3 +36,47 @@ exports.createCuti = async (req,res) => {
     
     res.json(cuti);
 }
+
+// UPDATE
+exports.updateCuti = async (req,res) => {
+    const id = req.params.id;
+
+    let cuti= await Cuti.findById(id);
+
+    if(!cuti){
+        return res.json({message: 'Data Not Found'});
+    }
+
+    const schema = {
+        nama_karyawan: 'string|optional',
+        tanggal: 'string|optional',
+        keterangan: 'string|optional'
+    }
+
+    const validate = v.validate(req.body, schema)
+
+    if(validate.length){
+        return res
+        .status(400)
+        .json(validate);
+    }
+
+    cuti= await cuti.update(req.body);
+    res.json(cuti);
+}
+
+exports.deleteCuti = async (req,res) => {
+    const id = req.params.id;
+
+    const cuti = await Cuti.findById(id);
+
+    if(!cuti){
+        return res.json({message: 'Data Not Found'});
+    }
+
+    await cuti.remove();
+
+    res.json({
+        message: 'Data Deleted'
+    });
+}
--- routes/CutiKaryawan.js
+++ routes/CutiKaryawan.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { readCuti, readCutibyId, createCuti } = require('../controller/cuti_controller');
+const { readCuti, readCutibyId, createCuti, updateCuti, deleteCuti } = require('../controller/cuti_controller');
 
 
 // READ API
@@ -11,5 +11,11 @@ router.get('/:id', readCutibyId)
 
 // CREATE API
 router.post('/', createCuti)
+
+// UPDATE API
+router.put('/:id', updateCuti)
+
+// DELETE API
+router.delete('/:id', deleteCuti)
 
 module.exports = router;
